83384b0d68e5fb184454da495743b41b93525367
Shivani Arbat <shivani.arbat@gmail.com>
2019-02-18 03:25:36 -0500

    created main activity view

---
+++ app/src/main/java/cs/uga/edu/restaurantlookup/ListRestaurants.java
@@ -0,0 +1,4 @@
+package cs.uga.edu.restaurantlookup;
+
+public class ListRestaurants {
+}
--- app/src/main/java/cs/uga/edu/restaurantlookup/MainActivity.java
+++ app/src/main/java/cs/uga/edu/restaurantlookup/MainActivity.java
@@ -2,7 +2,10 @@ package cs.uga.edu.restaurantlookup;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,9 +15,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         /* add the image view to display */
-        ImageView imageView = (ImageView) findViewById(R.id.mainPageImage);
+        ImageView imageView = findViewById(R.id.mainPageImage);
         imageView.setImageAlpha(50);
         imageView.setImageResource(R.drawable.food_main);
 
+        /* Text Views for the data */
+        TextView textViewAppTittle = findViewById(R.id.textViewAppTittle);
+        TextView textViewAppOverview = findViewById(R.id.textViewAppOverview);
+
+        /* Spinner */
+        Spinner spinnerCuisine = findViewById(R.id.spinnerCuisine);
+
+        /* Button - More Info */
+        Button buttonMoreInfo = findViewById(R.id.buttonMoreInfo);
+
+        /* Button - List Restaurants */
+        Button buttonListRestaurants = findViewById(R.id.buttonListRestaurants);
+
+        /* set default value for Spinner */
     }
 }
